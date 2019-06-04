001e2dae3094b9d3acf151b254aeb2990bf8c75f
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-04 17:25:38 +0100

    added 1 test for the create acount method

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,11 +1,50 @@
 package com.qa.persistence.domain;
 
 public class Account {
-	
-	//This class needs to have:
-	//An id
-	//An Account Number
-	//A First Name
-	//A last Name
-	
+
+	private String firstName;
+	private String lastName;
+	private int accountNumber;
+	private int id;
+
+	public Account(String firstName, String lastName, int accountNumber, int id) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -1,75 +1,81 @@
 package com.qa.MapTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.qa.persistence.domain.Account;
 import com.qa.persistence.repository.AccountMapRepository;
+import com.qa.util.JSONUtil;
 
 public class AccountServiceTest {
+	private JSONUtil json;
+	private AccountMapRepository am1;
 
 	@Before
 	public void setup() {
-	
+		json = new JSONUtil();
+		am1 = new AccountMapRepository();
 	}
-	
+
 	@Test
 	public void addAccountTest() {
-		fail("TODO");	
+		Account acc1 = new Account("K", "P", 1, 1);
+		String testAcc = json.getJSONForObject(acc1);
+		String message = am1.createAccount(testAcc);
+		assertEquals(message, am1.createAccount(testAcc));
 	}
-	
+
 	@Test
 	public void add2AccountsTest() {
-		fail("TODO");	
+		fail("TODO");
 	}
 
 	@Test
 	public void removeAccountTest() {
-		fail("TODO");	
+		fail("TODO");
 	}
-	
+
 	@Test
 	public void remove2AccountsTest() {
-		fail("TODO");	
+		fail("TODO");
 	}
-	
+
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
-		fail("TODO");	
+		fail("TODO");
 	}
-	
+
 	@Test
 	public void jsonStringToAccountConversionTest() {
 		// testing JSONUtil
-		fail("TODO");	
+		fail("TODO");
 	}
-
 
 	@Test
 	public void accountConversionToJSONTest() {
-		//testing JSONUtil
-		fail("TODO");	
+		// testing JSONUtil
+		fail("TODO");
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality
+		fail("TODO");
 	}
-	
+
 	@Test
 	public void getCountForFirstNamesInAccountWhenOne() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality
+		fail("TODO");
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenTwo() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality
+		fail("TODO");
 	}
 
 }
