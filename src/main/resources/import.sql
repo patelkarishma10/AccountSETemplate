f7a07ee4efb334ae6a7a1ee4ee6918206ffe7210
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-05 17:04:42 +0100

    added in ids in sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO ACCOUNT (firstName, lastName, accountNumber) VALUES ("John", "Doe", "1234")
-INSERT INTO ACCOUNT (firstName, lastName, accountNumber) VALUES ("Jane", "Dane", "1235")
-INSERT INTO ACCOUNT (firstName, lastName, accountNumber) VALUES ("Jim", "Taylor", "1236")
+INSERT INTO ACCOUNT (id, firstName, lastName, accountNumber) VALUES (1, 'John', 'Doe', '1234')
+INSERT INTO ACCOUNT (id, firstName, lastName, accountNumber) VALUES (2, 'Jane', 'Dane', '1235')
+INSERT INTO ACCOUNT (id, firstName, lastName, accountNumber) VALUES (3, 'Jim', 'Taylor', '1236')
